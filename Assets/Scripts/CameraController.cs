b9ae4f949dc44440c36394dd3e7dcecd98fee4af
Lucas Paiva <lucaspaiva@live.com>
2023-12-07 05:19:00 -0300

    implementa oclusão da câmera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -7,20 +6,35 @@ public class CameraController : MonoBehaviour
     public Transform player;
     public float verticalDistanceToPlayer = 1.5f;
     public float horizontalDistanceToPlayer = 3;
-    // Start is called before the first frame update
+    public float oclusionRaycastDistance = 5f;
+    public float lerpSpeed = 5f;
+    private RaycastHit hit;
+    private bool occlusionOccurring = false;
+
     void Start()
     {
         player = PlayerController.uniqueInstance.transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x, player.position.y + verticalDistanceToPlayer, player.position.z - horizontalDistanceToPlayer);
+        Vector3 defaultPosition = new Vector3(player.position.x, player.position.y + verticalDistanceToPlayer, player.position.z - horizontalDistanceToPlayer);
+
+        if (Physics.Raycast(player.position, (defaultPosition - player.position).normalized, out hit, oclusionRaycastDistance))
+        {
+            Vector3 targetPosition = hit.point;
+            targetPosition.y = player.position.y + verticalDistanceToPlayer;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * lerpSpeed);
+            occlusionOccurring = true;
+        }
+        else if (occlusionOccurring)
+        {
+            Vector3 targetPosition = defaultPosition;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * lerpSpeed);
+            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+                occlusionOccurring = false;
+        }
+        else
+            transform.position = defaultPosition;
     }
 }
